788e8517014e0b57bd7d6d03b2f486ffe2c814b2
Felix Otieno <omondifelixotieno2@gmail.com>
2024-11-16 13:02:08 +0300

    Add .gitattributes to normalize line endings

---
+++ test.js
@@ -0,0 +1,12 @@
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewTransaction(
+    "100",
+    "0xBcd4042DE499D14e55001CcbB24a551F3b954096",
+    "0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
+  ); 
+bitcoin.createNewBlock(); 
+
+console.log(bitcoin);
